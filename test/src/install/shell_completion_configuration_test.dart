ca273fc1d3b8cab7c91e55e50f5b9bd4db4b2a96
Renan <6718144+renancaraujo@users.noreply.github.com>
2022-11-22 21:41:23 +0000

    feat: list files when there is no completion on zsh (#21)

--- test/src/install/shell_completion_configuration_test.dart
+++ test/src/install/shell_completion_configuration_test.dart
@@ -36,7 +36,11 @@ if type compdef &>/dev/null; then
     IFS=\$'\n' reply=(\$(COMP_CWORD="\$((CURRENT-1))" COMP_LINE="\$BUFFER" COMP_POINT="\$CURSOR" very_good completion -- "\${words[@]}"))
     IFS=\$si
 
-    _describe 'values' reply
+    if [[ -z "\$reply" ]]; then
+        _path_files
+    else 
+        _describe 'values' reply
+    fi
   }
   compdef _very_good_completion very_good
 fi
