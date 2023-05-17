5bff0685087c70d46669be9ed9a10e31dc91da04
Alejandro Santiago <dev@alestiago.com>
2023-05-17 12:00:37 +0100

    test: included uninstallation test

---
+++ test/src/command_runner/commands/uninstall_completion_files_command_test.dart
@@ -0,0 +1,76 @@
+import 'package:cli_completion/cli_completion.dart';
+import 'package:cli_completion/installer.dart';
+import 'package:mason_logger/mason_logger.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class _MockLogger extends Mock implements Logger {}
+
+class _MockCompletionInstallation extends Mock
+    implements CompletionInstallation {}
+
+class _TestCompletionCommandRunner extends CompletionCommandRunner<int> {
+  _TestCompletionCommandRunner() : super('test', 'Test command runner');
+
+  @override
+  // ignore: overridden_fields
+  final Logger completionInstallationLogger = _MockLogger();
+
+  @override
+  final CompletionInstallation completionInstallation =
+      _MockCompletionInstallation();
+}
+
+void main() {
+  group('$UnistallCompletionFilesCommand', () {
+    late _TestCompletionCommandRunner commandRunner;
+
+    setUp(() {
+      commandRunner = _TestCompletionCommandRunner();
+    });
+
+    test('can be instantiated', () {
+      expect(UnistallCompletionFilesCommand<int>(), isNotNull);
+    });
+
+    test('is hidden', () {
+      expect(UnistallCompletionFilesCommand<int>().hidden, isTrue);
+    });
+
+    test('description', () {
+      expect(
+        UnistallCompletionFilesCommand<int>().description,
+        'Manually uninstalls completion files for the current shell.',
+      );
+    });
+
+    group('uninstalls completion files', () {
+      test('when normal', () async {
+        await commandRunner.run(['uninstall-completion-files']);
+
+        verify(
+          () => commandRunner.completionInstallationLogger.level = Level.info,
+        ).called(1);
+        verify(
+          () => commandRunner.completionInstallation
+              .uninstall(commandRunner.executableName),
+        ).called(1);
+      });
+
+      test('when verbose', () async {
+        await commandRunner.run(['uninstall-completion-files', '--verbose']);
+
+        verify(
+          () {
+            return commandRunner.completionInstallationLogger.level =
+                Level.verbose;
+          },
+        ).called(1);
+        verify(
+          () => commandRunner.completionInstallation
+              .uninstall(commandRunner.executableName),
+        ).called(1);
+      });
+    });
+  });
+}
