63487507fdc80e88082024a61236bea08b368f23
Alejandro Santiago <dev@alestiago.com>
2023-05-18 12:46:55 +0100

    test: added unmocked instances

--- test/src/command_runner/commands/uninstall_completion_files_command_test.dart
+++ test/src/command_runner/commands/uninstall_completion_files_command_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cli_completion/cli_completion.dart';
 import 'package:cli_completion/installer.dart';
 import 'package:mason_logger/mason_logger.dart';
@@ -27,6 +29,11 @@ void main() {
 
     setUp(() {
       commandRunner = _TestCompletionCommandRunner();
+
+      final completionInstallation = commandRunner.completionInstallation;
+      final completionInstallationFile = File('test-config.json');
+      when(() => completionInstallation.completionConfigurationFile)
+          .thenReturn(completionInstallationFile);
     });
 
     test('can be instantiated', () {
